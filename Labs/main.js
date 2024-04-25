aff22054c0d9274cd9cc528a4f02ac12a60b1529
bliem <Bliem718@students.ccsnh.edu>
2024-04-24 22:25:12 -0400

    Added Module 14 Lab Files.

--- Labs/main.js
+++ Labs/main.js
@@ -1,3 +1,42 @@
-var intro = document.getElementById("intro");
+var fullName = document.getElementById('fullname');
+var message = document.getElementById('message');
+var email = document.getElementById('email');
+var form = document.getElementById('contactform');
+var checkBox = document.getElementById('consent');
+var errors = document.getElementById('errors');
 
-intro.innerHTML = "Oh no! Where is my text?";
+fullName.addEventListener('focus', function() {
+    if (this.value == '') {
+        this.value = 'Please type your full name...';
+    }
+});
+message.addEventListener('focus', function() {
+    if (fullName.value != '') {
+        this.value = 'Hello ' + fullName.value + '! How can I help you today?'
+    }
+    else {
+        this.value = 'Please tell me who you are first. Enter your full name in the name field.'
+    }
+});
+
+form.addEventListener('submit', function(e) {
+    var stopSubmit = false;
+    if (!checkBox.checked) {
+        errors.innerHTML = '<p>You must consent to receiving emails!</p>';
+        stopSubmit = true;
+    }
+    if (fullName.value == '') {
+         errors.innerHTML += '<p>You must enter your name!</p>';
+        stopSubmit = true;
+    }
+    if (stopSubmit) {
+        e.preventDefault();
+    }
+});
+/*
+email.addEventListener('focus', function() {});
+
+fullName.addEventListener('blur', function() {});
+message.addEventListener('blur', function() {});
+email.addEventListener('blur', function() {});
+*/
